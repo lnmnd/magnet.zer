05f8c8387c1d78f55277ff345fe986857ba85865
lnmnd <lnmnd@users.noreply.github.com>
2014-10-27 15:45:56 +0100

    gehitu! txukundu

--- src/magnet/liburuak.clj
+++ src/magnet/liburuak.clj
@@ -9,12 +9,18 @@
   "Erabiltzailea egileen zerrenda string-etik aterata"
   (assoc era :egileak (read-string (:egileak era))))
 
+(defn- lortu-liburua [kon lib]
+  (->> (sql/query kon ["select id, magnet, erabiltzailea, titulua, egileak from liburuak where erabiltzailea=? and titulua=? and egileak=?" (:erabiltzailea lib) (:titulua lib) (:egileak lib)])
+       first
+       egileak-lortuta))
+
 (defn gehitu! [token edukia]
   (sql/with-db-connection [kon @konfig/db-kon]
     (let [egileak (prn-str (:egileak edukia))]
       (do (sql/insert! kon :liburuak
                        [:erabiltzailea :magnet :titulua :egileak]
                        [(token-erabiltzailea token) "magnet:?xt=urn:btih:TODO" (:titulua edukia) egileak])
-          [{:liburua
-            (egileak-lortuta (first (sql/query kon ["select id, magnet, erabiltzailea, titulua, egileak from liburuak where erabiltzailea=? and titulua=? and egileak=?" (token-erabiltzailea token) (:titulua edukia) egileak])))}
+          [{:liburua (lortu-liburua kon (assoc edukia
+                                          :erabiltzailea (token-erabiltzailea token)
+                                          :egileak egileak))}
            200]))))
