655fc09c235c18e5165097acdd9291dd93a650d0
lnmnd <lnmnd@users.noreply.github.com>
2014-10-29 19:53:20 +0100

    Iruzkinen eragiketak transakzio barnean burutu

--- src/magnet/iruzkinak.clj
+++ src/magnet/iruzkinak.clj
@@ -6,65 +6,68 @@
             [magnet.konfig :as konfig]))
 
 (defn- gehitu-iruzkina!
-  [edukia]
+  [kon edukia]
   (let [ir (assoc edukia :data (oraingo-data))]
-    (sql/with-db-connection [kon @konfig/db-kon]
-      (do (sql/insert! kon :iruzkinak
+    (do (sql/insert! kon :iruzkinak
                        [:liburua :erabiltzailea :data :edukia]
                        [(:liburua edukia) (:erabiltzailea edukia) (:data edukia) (:edukia edukia)])
-          (assoc edukia :id (:id (first (sql/query kon "select identity() as id"))))))))
+          (assoc edukia :id (:id (first (sql/query kon "select identity() as id")))))))
 
 (defn- lortu-iruzkina
-  [id]
-  (first (sql/query @konfig/db-kon ["select * from iruzkinak where id=?" id])))
+  [kon id]
+  (first (sql/query kon ["select * from iruzkinak where id=?" id])))
 
 (defn- aldatu-iruzkina!
-  [id edukia]
+  [kon id edukia]
   (sql/update! @konfig/db-kon :iruzkinak
                {:edukia (:edukia edukia)}
                ["id=?" id]))
 
 (defn- ezabatu-iruzkina!
-  [id]
-  (sql/delete! @konfig/db-kon :iruzkinak ["id=?" id]))
+  [kon id]
+  (sql/delete! kon :iruzkinak ["id=?" id]))
 
 (defn gehitu!
   "id liburuarekin lotutako iruzkina gehitu."
   [token id edukia]
-  (if-let [{erabiltzailea :erabiltzailea} (lortu-saioa token)]
-    [{:iruzkina
-      (gehitu-iruzkina! (assoc edukia
-                          :liburua id
-                          :erabiltzailea erabiltzailea))}
-     200]
-    [{} 401]))
+  (sql/with-db-transaction [kon @konfig/db-kon]
+    (if-let [{erabiltzailea :erabiltzailea} (lortu-saioa token)]
+      [{:iruzkina
+        (gehitu-iruzkina! kon (assoc edukia
+                                :liburua id
+                                :erabiltzailea erabiltzailea))}
+       200]
+      [{} 401])))
 
 (defn aldatu!
   "Iruzkinaren edukia aldatzen du."
   [token id edukia]
-  (if-let [ir (lortu-iruzkina id)]
-    (if (= (:erabiltzailea (lortu-saioa token))
-           (:erabiltzailea ir))
-      (do (aldatu-iruzkina! id edukia)
-          [{:iruzkina (assoc ir :edukia (:edukia edukia))}
-           200])
-      [{} 401])
-    [{} 404]))
+  (sql/with-db-transaction [kon @konfig/db-kon]
+    (if-let [ir (lortu-iruzkina kon id)]
+      (if (= (:erabiltzailea (lortu-saioa token))
+             (:erabiltzailea ir))
+        (do (aldatu-iruzkina! kon id edukia)
+            [{:iruzkina (assoc ir :edukia (:edukia edukia))}
+             200])
+        [{} 401])
+      [{} 404])))
 
 (defn lortu
   "id jakineko iruzkina lortzen du."
   [id]
-  (if-let [ir (lortu-iruzkina id)]
-    [{:iruzkina ir} 200]
-    [{} 404]))
+  (sql/with-db-transaction [kon @konfig/db-kon]
+    (if-let [ir (lortu-iruzkina kon id)]
+      [{:iruzkina ir} 200]
+      [{} 404])))
 
 (defn ezabatu!
   "Iruzkina ezabatzen du."
   [token id]
-  (if-let [ir (lortu-iruzkina id)]
-    (if (= (:erabiltzailea (lortu-saioa token))
-           (:erabiltzailea ir))
-      (do (ezabatu-iruzkina! id)
-          [{} 200])
-      [{} 401])
-    [{} 404]))
+  (sql/with-db-transaction [kon @konfig/db-kon]
+    (if-let [ir (lortu-iruzkina kon id)]
+      (if (= (:erabiltzailea (lortu-saioa token))
+             (:erabiltzailea ir))
+        (do (ezabatu-iruzkina! kon id)
+            [{} 200])
+        [{} 401])
+      [{} 404])))
