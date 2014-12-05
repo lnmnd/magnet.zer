57ad15af4532963990c868acedb39c69dd5f48b3
lnmnd <lnmnd@users.noreply.github.com>
2014-12-05 10:08:11 +0100

    konfig dokumentatu

--- src/clj/magnet/konfig.clj
+++ src/clj/magnet/konfig.clj
@@ -1,18 +1,28 @@
 (ns magnet.konfig
   (:require [magnet.konfiglehenetsia :as l]))
 
-(def db-kon (atom l/db-kon))
+(def ^{:doc "Datubasearen konexio parametroak, dbspec gisa.
+  Adb:
+  {:classname \"org.h2.Driver
+   :subprotocol \"h2\"
+   :subname \"jdbc:h2:magnet\"}"}
+  db-kon (atom l/db-kon))db-kon
 
-; bildumen muga
-(def muga (atom l/muga))
+(def ^{:doc "Bilduma batean agertuko diren elementu kopurua."}
+  muga (atom l/muga))
 
-; denbora segunduetan
-(def saio-iraungitze-denbora (atom l/saio-iraungitze-denbora))
+(def ^{:doc "Saioaren iraungitze denbora, segundutan."}
+  saio-iraungitze-denbora (atom l/saio-iraungitze-denbora))
 
-(def partekatu (atom l/partekatu))
+(def ^{:doc "Torrentak partekatu nahi diren edo ez."}
+  partekatu (atom l/partekatu))
 (def epub-karpeta (atom l/epub-karpeta))
 (def torrent-karpeta (atom l/torrent-karpeta))
 (def irudi-karpeta (atom l/irudi-karpeta))
 (def irudi-url (atom l/irudi-url))
-(def trackerrak (atom l/trackerrak))
-(def torrent-gehitze-programa (atom l/torrent-gehitze-programa))
+(def  ^{:doc "Trackerren zerrenda.
+  Lehenengoa torrentaren tracker gisa ezarriko da.
+  Gainontzekoak magnet loturak agertuko dira."}
+  trackerrak (atom l/trackerrak))
+(def ^{:doc "Torrent fitxategia eta katalogoa emanda torrenta partekatze duen programa."}
+  torrent-gehitze-programa (atom l/torrent-gehitze-programa))
