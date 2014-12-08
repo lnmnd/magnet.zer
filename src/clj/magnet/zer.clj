00e4e6ab1d942b838adfd1b6cd8da8919db687a4
lnmnd <lnmnd@users.noreply.github.com>
2014-12-08 10:58:01 +0100

    zer txukundu

--- src/clj/magnet/zer.clj
+++ src/clj/magnet/zer.clj
@@ -8,21 +8,21 @@
   [portua handler]
   {:portua portua
    :handler handler
-   :jetty (atom nil)})
+   :http (atom nil)})
 
 (defn hasi
   "Zerbitzaria abiarazten du."
   [zer]
-  (when-not @(:jetty zer)
-    (println "Zerbitzaria" (:portua zer) " portuan abiarazten")
-    (swap! (:jetty zer) (fn [_] (run-jetty (:handler zer) {:port (:portua zer) :join? false})))
+  (when-not @(:http zer)
+    (println ";; Zerbitzaria" (:portua zer) " portuan abiarazten")
+    (reset! (:http zer) (run-jetty (:handler zer) {:port (:portua zer) :join? false}))
     (when @konfig/partekatu
       (torrent/katalogoko-torrentak-partekatu! @konfig/torrent-karpeta))))
 
 (defn geratu
   "Zerbitzaria geratzen du."
   [zer]
-  (when @(:jetty zer)
-    (println "Zerbitzaria geratzen")
-    (.stop @(:jetty zer))
-    (swap! (:jetty zer) (fn [_] nil))))
+  (when @(:http zer)
+    (println ";; Zerbitzaria geratzen")
+    (.stop @(:http zer))
+    (reset! (:http zer) nil)))
