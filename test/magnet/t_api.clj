6df445a6b100e7a0c5e04b5c933d7f873627706d
lnmnd <lnmnd@users.noreply.github.com>
2014-12-21 22:28:05 +0100

    merge erabili proben konfigurazioa sortzeko

--- test/magnet/t_api.clj
+++ test/magnet/t_api.clj
@@ -9,16 +9,16 @@
 
 (def test-konfig
   (let [konfig (eval (read-string (slurp "konfig.clj")))]
-    (assoc konfig
-      :portua 3001
-      :db-kon {:classname "org.h2.Driver"
-               :subprotocol "h2"
-               :subname "jdbc:h2:test"}    
-      :partekatu false
-      :kokapenak {:epub-karpeta "test-resources/private/torrent/"
-                  :torrent-karpeta "test-resources/private/torrent/"
-                  :irudi-karpeta "test-resources/public/img/"
-                  :irudi-url "http://localhost:3001/img/"})))
+    (merge konfig
+           {:portua 3001
+            :db-kon {:classname "org.h2.Driver"
+                     :subprotocol "h2"
+                     :subname "jdbc:h2:test"}    
+            :partekatu false
+            :kokapenak {:epub-karpeta "test-resources/private/torrent/"
+                        :torrent-karpeta "test-resources/private/torrent/"
+                        :irudi-karpeta "test-resources/public/img/"
+                        :irudi-url "http://localhost:3001/img/"}})))
 
 (defonce zerbitzaria (sortu test-konfig (handler-sortu test-konfig (sortu-saioak))))
 
